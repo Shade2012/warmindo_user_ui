4a8497acde04d408c3bba3881e5fd39b0c15e9c4
AkbarFelda <akbarfelda65@gmail.com>
2024-04-05 11:00:54 +0700

    feat: [AF] Add splash page

--- lib/pages/splash_page/controller/splash_controller.dart
+++ lib/pages/splash_page/controller/splash_controller.dart
@@ -1,5 +1,41 @@
+import 'dart:async';
 import 'package:get/get.dart';
+import 'package:warmindo_user_ui/routes/AppPages.dart';
 
 class SplashController extends GetxController {
+  final _animationController = StreamController<double>.broadcast();
+  final _circleSizeController = StreamController<double>.broadcast();
 
+  Stream<double> get animationStream => _animationController.stream;
+  Stream<double> get circleSizeStream => _circleSizeController.stream;
+
+  @override
+  void onInit() {
+    super.onInit();
+    animate();
+  }
+
+  void animate() async {
+    await Future.delayed(Duration(seconds: 1));
+    _animationController.sink.add(0.3); // Green Circle
+    _circleSizeController.sink.add(0.3); // Small size
+    await Future.delayed(Duration(seconds: 1));
+    _animationController.sink.add(0.7); // Logo Circle
+    _circleSizeController.sink.add(0.7); // Medium size
+    await Future.delayed(Duration(seconds: 1));
+    _animationController.sink.add(0.6); // Orange Circle
+    _circleSizeController.sink.add(0.6); // Medium size
+    await Future.delayed(Duration(seconds: 1));
+    _animationController.sink.add(1);   // Red Circle
+    _circleSizeController.sink.add(1);   // Large size
+    await Future.delayed(Duration(milliseconds: 400)); //buat hide
+    Get.offNamed(Routes.ONBOARD_PAGE); 
+  }
+
+  @override
+  void onClose() {
+    _animationController.close();
+    _circleSizeController.close();
+    super.onClose();
+  }
 }
--- lib/pages/splash_page/view/splash_page.dart
+++ lib/pages/splash_page/view/splash_page.dart
@@ -1,13 +1,54 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:warmindo_user_ui/pages/splash_page/controller/splash_controller.dart';
+import 'package:warmindo_user_ui/utils/themes/color_themes.dart';
+import 'package:warmindo_user_ui/utils/themes/image_themes.dart';
 
 class SplashPage extends StatelessWidget {
-  const SplashPage({super.key});
+  const SplashPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-    body: Center(
-      child: Text('Splash Screen'),
-    ));
+    final SplashController _splashController = Get.find();
+    return Scaffold(
+      body: StreamBuilder<double>(
+        stream: _splashController.animationStream,
+        builder: (context, snapshot) {
+          return StreamBuilder<double>(
+            stream: _splashController.circleSizeStream,
+            builder: (context, sizeSnapshot) {
+              final double circleSize = sizeSnapshot.data ?? 1.0;
+              final double animationValue = snapshot.data ?? 0.0;
+
+              return Container(
+                color: animationValue == 1 ? ColorResources.splashGradientEnd : Colors.transparent,
+                child: Center(
+                  child: AnimatedContainer(
+                    duration: Duration(seconds: 1),
+                    height: MediaQuery.of(context).size.height * circleSize,
+                    width: MediaQuery.of(context).size.width * circleSize,
+                    decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      color: animationValue == 0.3
+                          ? ColorResources.splashGradientStart // Hijau
+                          : animationValue == 0.6 // Oranye
+                              ? ColorResources.splashGradientMid // Oranye
+                              : Colors.transparent, // Transparan untuk logo
+                    ),
+                    child: animationValue == 0.7 // Tampilkan logo ketika mencapai 0.7
+                        ? Image.asset(
+                            Images.logo,
+                            height: MediaQuery.of(context).size.height * circleSize,
+                            width: MediaQuery.of(context).size.width * circleSize,
+                          )
+                        : null,
+                  ),
+                ),
+              );
+            },
+          );
+        },
+      ),
+    );
   }
 }
